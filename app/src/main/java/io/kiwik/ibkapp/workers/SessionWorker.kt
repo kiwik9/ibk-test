ab8228939259b271bb0fa8cde8221d612f766fd1
kiwik <jusepe600@msn.com>
2022-08-12 12:36:50 -0500

    Se agrego un pendingIntent para que cuando el usuario este navegando y acabe tiempo de sesion se mueva hacia el loginActivity

--- app/src/main/java/io/kiwik/ibkapp/workers/SessionWorker.kt
+++ app/src/main/java/io/kiwik/ibkapp/workers/SessionWorker.kt
@@ -1,21 +1,31 @@
 package io.kiwik.ibkapp.workers
 
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.work.Worker
 import androidx.work.WorkerParameters
-import io.kiwik.domain.repository.auth.AuthRepository
+import io.kiwik.domain.interactors.SignOutUseCase
 import io.kiwik.domain.util.ResponseStatus
+import io.kiwik.ibkapp.ui.login.LoginActivity
 import kotlinx.coroutines.runBlocking
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
-class SessionWorker(appContext: Context, workerParams: WorkerParameters) :
-    Worker(appContext, workerParams) {
+
+class SessionWorker(private val appContext: Context, workerParams: WorkerParameters) :
+    Worker(appContext, workerParams), KoinComponent {
+
+    private val signOutUseCase by inject<SignOutUseCase>()
 
     override fun doWork(): Result {
 
         return runBlocking {
-            val authRepository = AuthRepository()
-            val result = authRepository.signOut()
+            val result = signOutUseCase.execute()
+            val intent = Intent(appContext, LoginActivity::class.java)
+            val pendingIntent = PendingIntent.getActivity(appContext, 1, intent, PendingIntent.FLAG_IMMUTABLE)
             if (result.responseStatus == ResponseStatus.SUCCESS) {
+                pendingIntent.send()
                 return@runBlocking Result.success()
             } else {
                 return@runBlocking Result.failure()
